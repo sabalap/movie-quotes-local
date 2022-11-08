533bc9285ddc76c8f33e56c70991f9340d3cd146
Saba <saba.lapanashvili15@gmail.com>
2022-11-08 14:41:16 +0400

    feat: add migrations and custom data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,75 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Movie;
+use App\Models\Quote;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$movie1 = Movie::factory()->create([
+			'title' => 'The Pursuit Of Happyness',
+			'image' => './images/the-pursuit-of-happyness',
+		]);
+		$movie2 = Movie::factory()->create([
+			'title' => 'Spiderman',
+			'image' => './images/spiderman',
+		]);
+		$movie3 = Movie::factory()->create([
+			'title' => 'Batman',
+			'image' => './images/batman',
+		]);
+		$movie4 = Movie::factory()->create([
+			'title' => 'Shawshank',
+			'image' => './images/shawshank',
+		]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+		Quote::factory()->create([
+			'movie_id' => $movie1->id,
+			'quote'    => "It's okay to fail; it's not okay to quit.",
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie1->id,
+			'quote'    => "Don't just talk that talk, walk it and go forward.",
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie2->id,
+			'quote'    => "I've just got to do this on my own.",
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie2->id,
+			'quote'    => 'Mary Jane Watson: Who are you?',
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie3->id,
+			'quote'    => 'Your anger gives you great power.',
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie3->id,
+			'quote'    => 'The night is darkest just before the dawn.',
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie4->id,
+			'quote'    => "There's not a day goes by I don't feel regret.",
+		]);
+
+		Quote::factory()->create([
+			'movie_id' => $movie4->id,
+			'quote'    => 'The world went and got itself in a big damn hurry.',
+		]);
+	}
 }
