fcb6587edb8abf2d28c0780e0f2a4e786a76d890
Saba <saba.lapanashvili15@gmail.com>
2022-11-19 22:57:24 +0400

    feat: favicon

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,7 +1,4 @@
 <x-layout>
-    <button class="ml-5 w-10 h-10 bg-white mt-5 rounded">
-        <a href={{route("home")}}>{{__("language.back")}}</a>
-    </button>
     <div class="text-center mx-auto w-5/12 mt-20 inline-block">
         <h1 class="mb-8 text-left text-4xl mr-8 text-white">{{$movie->title}}</h1>
         @foreach ($quotes as $quote)
